eb577514b76acfed031d5dd7084d04b3e63139e2
Adwaith jayan <aj007boss@gmail.com>
2024-12-10 23:03:05 +0530

    project completed

---
+++ frontend/src/App.jsx
@@ -0,0 +1,43 @@
+import { Routes,Route, Navigate } from "react-router-dom";
+
+import Navbar from "./components/navbar";
+import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import Settings from "./pages/Settings";
+import Profile from "./pages/Profile";
+import {Loader} from 'lucide-react'
+import { useAuthStore } from "./store/useAuthStore";
+import { useEffect } from "react";
+
+import { Toaster } from 'react-hot-toast';
+import { useThemeStore } from "./store/useThemeStore";
+
+
+export default function App() {
+  const {theme}=useThemeStore()
+  const {authUser,checkAuth,isCheckingAuth} = useAuthStore();
+  useEffect(()=>{
+    checkAuth();
+  },[checkAuth])
+
+  if(isCheckingAuth && !authUser)return(
+    <div className="flex items-center justify-center h-screen">
+      <Loader className='size-10 animate-spin'/>
+    </div>
+  )
+
+  return (
+    <div data-theme={theme}>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={authUser ?<Home/>:<Navigate to="/login"/>}/>
+        <Route path="/signup" element={!authUser ?<Signup/>:<Navigate to="/"/>}/>
+        <Route path="/login" element={!authUser ?<Login/>:<Navigate to='/'/>}/>
+        <Route path="/settings" element={<Settings/>}/>
+        <Route path="/profile" element={authUser ?<Profile/>:<Navigate to="/login"/>}/>
+      </Routes>
+      <Toaster />
+    </div>
+  )
+}
